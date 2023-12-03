c1533467dafc9bc7259de59c20140e0e0542d165
adrianthuo <adrianthuo@gmail.com>
2023-12-03 15:04:40 +0300

    py-06

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,6 @@
 for d1 in range(10):
     for t2 in range(d1 + 1, 10):
-        print("{}{}, ".format(d1,t2),end='')
+        print("{}{}".format(d1, t2), end=', ' if not (d1 == 8 and t2 == 9) else '')
+
+# Print a newline after the inner loop
+print()
