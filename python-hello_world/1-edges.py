2d0960d63d9f40eae2f0948e96855a09b51282df
adrianthuo <adrianthuo@gmail.com>
2023-12-03 12:20:37 +0300

    py-01

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,5 @@
 word = "Holberton"
 
-print("First 3 letters: " , word[:3])
-print("Last 2 letters: " ,word[-2:])
-print("Middle word: ",word[1:8])
+print("First 3 letters:" , word[:3])
+print("Last 2 letters:" ,word[-2:])
+print("Middle word:",word[1:8])
